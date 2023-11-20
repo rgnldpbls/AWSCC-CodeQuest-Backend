51593370febc4095ce68cf7e72fb619c3b45ee08
Regienald Pueblos <regienaldpueblos13@gmail.com>
2023-11-20 23:28:07 +0800

    finished day 1 task

--- backend/day-01/test.py
+++ backend/day-01/test.py
@@ -1,6 +1,9 @@
-name = None
-age = None
-isMale = None
-department = None
+name = 'Regienald Pueblos'
+age = 20
+isMale = True
+department = 'Department of Software and Web Development'
+about_me = 'I am boring person'
 
 print(f"{name} is an {age}-year old {'male' if isMale else 'female'} participating in {department}'s 30 Days of Coding.")
+
+print(f"Hello, I am {name}. I love {department} and I love being in the Backend even more!\nOne thing about me is that {about_me}.")
