7836dc4cbf828963d1af5943ebe275c1fb5b0299
Regienald Pueblos <regienaldpueblos13@gmail.com>
2023-11-21 22:01:32 +0800

    finished day 2 task

---
+++ backend/day-02/day-2.py
@@ -0,0 +1,6 @@
+numOne = int(input("Enter the first number: "))
+numTwo = int(input("Enter the second number: "))
+numThree = int(input("Enter the third number: "))
+
+sum = numOne + numTwo + numThree
+print(f"\nSum: {sum}")
