af04c5c970c6e7556492a5d984bf26690a5058b4
Regienald Pueblos <regienaldpueblos13@gmail.com>
2023-11-27 21:41:59 +0800

    finished day 8 task

--- backend/day-08/day-8-avg.py
+++ backend/day-08/day-8-avg.py
@@ -1,10 +1,16 @@
+def avg(numList):
+    sum = 0
+    avg = 0
+
+    for num in my_list:
+        sum += num
+
+    avg = sum / len(my_list)
+    return avg
+
 my_list = [5, 9, 2, 3, 1]
+average = avg(my_list)
 
-sum = 0
-avg = 0
 
-for num in my_list:
-    sum += num
 
-avg = sum / len(my_list)
-print(avg)
+print(average)
