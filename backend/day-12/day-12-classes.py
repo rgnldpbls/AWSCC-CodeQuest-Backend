20431f1e1c2956b49ba9875e6b31fefaac206d63
Regienald Pueblos <regienaldpueblos13@gmail.com>
2024-02-05 14:21:02 +0800

    finished day 12 task

--- backend/day-12/day-12-classes.py
+++ backend/day-12/day-12-classes.py
@@ -1,13 +1,21 @@
 class Student:
-    def __init__(self, name, age, 
-        is_enrolled, classes, offenses):
+    def __init__(self, name, age, is_enrolled, classes, offenses):
         self.name = name
         self.age = age
         self.is_enrolled = is_enrolled
         self.classes = classes
         self.offenses = offenses
+    
+    def addClass(self, newClass):
+        self.classes.append(newClass)
+
+    def addOffense(self, newOffense):
+        self.offenses.append(newOffense)
 
 student1 = Student("Juan Dela Cruz", 20, True, [], [])
+student1.addClass('English')
+student1.addClass('Mathematics')
+student1.addOffense('Late')
 
 print(f"Name: {student1.name}")
 print(f"Age: {student1.age}")
