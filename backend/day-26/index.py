a09425d921e45f943f91d9f293c8b5888152fabe
Regienald Pueblos <regienaldpueblos13@gmail.com>
2024-02-21 00:00:37 +0800

    finished day 26 task

---
+++ backend/day-26/index.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///new_books.db'
+db = SQLAlchemy(app)
+
+class Books(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), unique=True, nullable=False)
+    author = db.Column(db.String(120), unique=True, nullable=False)
+    published_year = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f'<Book {self.title}--{self.author}>'
+    
+db.create_all()
+
+new_book = Book(title="To kill a Mockingbird", author="Harper Lee", published_year=1960)
+db.session.add(new_book)
+db.session.commit()
+
+books = Book.query.all()
